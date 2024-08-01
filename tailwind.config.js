f2357753eadab8daf9871e57641265afc7d104bf
Mohamedabdelhany <88891008+Mohamedabdelhany@users.noreply.github.com>
2024-08-01 07:22:57 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { withRouter } = require("next/router");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,8 +13,18 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hero: "url('/images/slider-bg.jpg')",
       },
     },
+    colors: {
+      primary: "#0a97b0",
+      secondary: "#04233b",
+      white: "#fff",
+      black: "#000",
+      gray: "#909090",
+      subgray: "#f8f8f9",
+    },
   },
+
   plugins: [],
 };
